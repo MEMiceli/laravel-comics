75088992c8691e85fe747377b62b3a93d6e5bdd9
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-07-07 18:23:13 +0200

    lentamente...

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="right_header">
+      <div class="box_image">
+          <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+      </div>    
+    </div>
+    <div class="left_header">
+      <nav class="nav_header">
+          <ul>
+              <li><a href="#"></a>Characters</li>
+              <li class="active"><a href="#"></a>Comics</li>        
+              <li><a href="#"></a>Movies</li>
+              <li><a href="#"></a>Tv</li>
+              <li><a href="#"></a>Games</li>
+              <li><a href="#"></a>Collectibles</li>
+              <li><a href="#"></a>Videos</li>
+              <li><a href="#"></a>Fans</li>
+              <li><a href="#"></a>News</li>
+              <li><a href="#"></a>Shop</li>        
+          </ul>
+      </nav>     
+    </div>
+</header>
